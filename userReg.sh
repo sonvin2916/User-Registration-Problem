1efdb1f493162f6a50faf6775f72480f48194b51
sonvin2916 <sonalchougule07@gmail.com>
2020-06-16 22:25:21 +0530

    UC-6 : Added

--- userReg.sh
+++ userReg.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 echo "Enter password"
 read pwd
-pattern="^([A-Za-z0-9@!#$]){8,}$"
+pattern="^([A-Za-z0-9@!#]*)[[:upper:]]+([A-Za-z0-9@!#]*)$"
+
 if [[ $pwd =~ $pattern  ]]
 then
 	echo "Valid"
 else
-	echo "Password should cointain minimum 8 characters"
+	echo "Invalid"
 fi
 
