e2fda23a19175986d3b458fdc18132e4d4fc3fb9
sonvin2916 <sonalchougule07@gmail.com>
2020-06-16 17:05:00 +0530

    UC-1 : Added

--- userReg.sh
+++ userReg.sh
@@ -1,1 +1,11 @@
-echo "Welcome"
+#! /bin/bash
+echo "Enter first name"
+read firstName
+pattern="^[[:upper:]]+([a-z]{3,})"
+if [[ $firstName =~ $pattern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
+
